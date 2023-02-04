bb76613ef77468c2bf5a489b09ada5e4bc40f884
jrichard1909 <jrichard1909@gmail.com>
2023-02-04 12:20:41 -0500

    add task 4

---
+++ module1_task4/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Update package manager
+apt-get update
+
+# Install 'make' and 'wget'
+apt-get install -y make wget
+
+# Download and extract appropriate version of GoHugo for Ubuntu 18.04
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
+tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
+
+# Move the executable to a directory in the PATH
+mv hugo /usr/local/bin/
+
+# Remove unnecessary files
+rm hugo_extended_0.109.0_Linux-64bit.tar.gz
+
+# Build the website
+make build
